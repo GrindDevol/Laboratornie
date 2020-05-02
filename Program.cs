66bc4bcd1c59a21b6c40b6e29f6463276511b22b
GrindDevol <60744242+GrindDevol@users.noreply.github.com>
2020-05-02 14:33:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BP_labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Hello World!");
+            Console.WriteLine("hello Hello World!");
+        }
+    }
+}
